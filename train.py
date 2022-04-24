b6c7525d37bdbef3f64b3ae15c21cf41f48667c4
yang <yangcheng@zuzuche.com>
2022-04-24 10:45:49 +0800

    调试训练过程日志.

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def main():
     yolov5_type = "5l"
     image_shape = (640, 640, 3)
     num_class = 91
-    batch_size = 4
+    batch_size = 6
     # -1表示全部数据参与训练
     train_img_nums = -1
 
@@ -71,7 +71,7 @@ def main():
 
     # data = coco_data.next_batch()
     for epoch in range(epochs):
-        if epoch % 10 == 0 and epoch != 0:
+        if epoch % 20 == 0 and epoch != 0:
             yolov5.save_weights(log_dir + '/yolov5-tf-{}.h5'.format(epoch))
             # 保存为pb格式
             # yolov5.save(log_dir + '/yolov5-tf-{}.pb'.format(epoch), save_format='tf')
