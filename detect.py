f8e8211e7591f4ad35df1868f9fff373d2e90829
yang <yangcheng@zuzuche.com>
2022-04-28 14:03:06 +0800

    修复检测时nms越界的bug.

--- detect.py
+++ detect.py
@@ -17,13 +17,15 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 def main():
     model_path = "h5模型路径, 默认在 ./logs/yolov5-tf-300.h5"
+    # model_path = "./logs/yolov5-tf-300.h5"
     image_path = "提供你要测试的图片路径"
+    # image_path = "./data/tmp/JPEGImages/Cats_Test0.jpg"
     image = cv2.imread(image_path)
     # 可以选择 ['5l', '5s', '5m', '5x'], 跟随训练
     yolov5_type = "5l"
     image_shape = (640, 640, 3)
     num_class = 91
-    # num_class = 2
+    # num_class = 3
     batch_size = 1
 
     # 这里anchor归一化到[0,1]区间
@@ -56,7 +58,7 @@ def main():
                'dining table', 'none', 'none', 'toilet', 'none', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
                'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'none', 'book', 'clock',
                'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-    # classes = ['cat', 'dog']
+    # classes = ['_background_', 'cat', 'dog']
 
     yolo = Yolo(
         model_path=model_path,
@@ -74,7 +76,7 @@ def main():
     predicts = yolo.predict(image)
     if predicts.shape[0]:
         pred_image = image.copy()
-        for box_obj_cls in predicts:
+        for box_obj_cls in predicts[0]:
             if box_obj_cls[4] > 0.5:
                 label = int(box_obj_cls[5])
                 class_name = classes[label]
